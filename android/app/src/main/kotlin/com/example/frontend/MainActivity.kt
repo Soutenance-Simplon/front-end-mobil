b15a35b1de149f585520710f24de6226b683357c
RokhyatouSF <rokhyatousarrfall@gmail.com>
2026-09-16 12:25:43 +0000

    chore: initialisation du projet Flutter et configuration pubspec.yaml

---
+++ android/app/src/main/kotlin/com/example/frontend/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.sante.frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+import 'firebase_options.dart';
+import 'routes/app_router.dart';
+import 'core/theme/app_theme.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends ConsumerWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return MaterialApp.router(
+      title: 'DIAM YARAAM SANTÉ',
+      debugShowCheckedModeBanner: false,
+
+      theme: AppTheme.lightTheme,
+
+      routerConfig: appRouter,
+
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('fr', 'FR'),
+        Locale('en', 'US'),
+      ],
+    );
+  }
+}
